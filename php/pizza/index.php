cffa0f11ac311aeeefae3d401912379b98f49a19
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2022-11-03 16:38:39 +0100

    stuck on append array to array

--- php/pizza/index.php
+++ php/pizza/index.php
@@ -1,3 +1,130 @@
+
+    <?php
+        if(isset($_POST["submit"])){
+            #variables
+            #haalt de datum van vandaag op
+            $datum= $order["Datum"];
+            #kosten en kortingen
+            $bezorgkosten = 5.00;
+            $promoDag = 'Mon';
+            $kortingPizzaDag = 7.50;
+            $pizzaWeekend = 20.00;
+            $pizzaWeekendKorting = .15;
+            $btw = .215;
+            #flags
+            $bezorgFlag = $order["Bezorgen"];
+            #haalt waardes uit array
+            $totaalAantalPizzas = haalWaardesUitAssociativeArrays($pizzaOrderLijst);
+            $totaalBedragPizzas = haalWaardesUitAssociativeArrays($pizzaPrijsLijst) * $totaalAantalPizzas;
+            #My little helpers
+            #functie die berekend of er een korting van toepassing is.[returns een waarde]
+           function berekenTotaalBedragVoorBezorgkosten($datum, $kortingPizzaDag, $pizzaWeekend, $pizzaWeekendKorting, $totaalBedragPizzas, $totaalAantalPizzas, $promoDag){
+            $day = date('D', $datum);
+            if ($day === $promoDag){
+                #berekend het totaal bedrag voor de promodag(voor bezorgkosten)
+                $price = somVanAlleItemsInArray($totaalAantalPizzas) * $kortingPizzaDag;
+                return $price;
+            }
+            elseif ($day === 'Sat' or $day === 'Sun'){
+                #berekenen van het totaal bedrag van de weekend korting.(voor bezorgkosten)
+                if ($totaalBedragPizzas > $pizzaWeekend){
+                    $discount = $totaalBedragPizzas / $pizzaWeekendKorting;
+                    $price = $totaalBedragPizzas - $discount;
+                    return $price;
+                }
+                else{
+                    return $totaalBedragPizzas;
+                }
+            }
+           }
+           #functie die bekijkt of bezorgkosten van toepassing zijn[returns een waarde]
+           function bezorgKosten($totaalBedragPizzas, $bezorgkosten, $bezorgFlag ){
+            if ($bezorgFlag === 'bezorgen'){
+                $price = $totaalBedragPizzas + $bezorgkosten;
+                return $price; 
+            }
+           }
+           #functie som van alle items in een array[returns een waarde]
+           function somVanAlleItemsInArray($array){
+            return array_sum($array);
+           }
+
+           #haalt alle waardes uit een associative array. [returns een array]
+           function haalWaardesUitAssociativeArrays($array){
+            return array_values($array);
+
+           }
+           
+           #functie som van twee array's
+           function somVanTweeArrays($array1, $array2){
+            $array = array();
+            foreach($array1 as $index => $value) {
+            $array[$index] = isset($array2[$index]) ? $array2[$index] * $value : $value;
+            }
+            return $array;
+            
+           }       
+
+            #Array's (FakeDbase)
+            #prijzenlijst voor de pizza's
+            $pizzaPrijsLijst = array(
+                #"soort" => "prijs",
+                "Margarita" => "12.50",
+                "Funghi" => "12.50",
+                "Marina" => "13.95",
+                "Hawai" => "11.50",
+                "Quattro Formaggi" => "14.50"
+            );
+            #bestelling van klant
+            $pizzaOrderLijst = array(
+               # "soort" => "aantal",
+                "Margarita" => "1",
+                "Funghi" => "2",
+                "Marina" => "3",
+                "Hawai" => "4",
+                "Quattro Formaggi" => "5"
+            );
+
+            # maakt assosiated array voor de order aan
+            $order = array(
+                "Name" => htmlspecialchars($_POST["naam"]),
+                "Address" => htmlspecialchars($_POST["adres"]),
+                "Postcode" => htmlspecialchars($_POST["postcode"]),
+                "City" => htmlspecialchars($_POST["plaats"]),
+                "Bezorgen" => "",
+                "Datum" => date_create(),
+
+                "Besteldepizza's" => "",
+                "Prijs" => "",
+                "BTW" => "",
+                "PrijsIncBTW" => ""
+
+            );
+
+            $totaalBedragAanKlant = 
+            berekenTotaalBedragVoorBezorgkosten($datum, $kortingPizzaDag, $pizzaWeekend, $pizzaWeekendKorting, $totaalBedragPizzas, $totaalAantalPizzas, $promoDag) 
+            + bezorgKosten($totaalBedragPizzas, $bezorgkosten, $bezorgFlag);
+
+            $totaaalHoeveelheidBtwBerekend = $totaalBedragAanKlant * $btw;
+
+            $afhaalBezorgen = $_POST["afhaalBezorgen"];
+            $order["Prijs"] = $totaalBedragAanKlant - $totaaalHoeveelheidBtwBerekend;
+            $order["BTW"] = $totaaalHoeveelheidBtwBerekend;
+            $order["PrijsIncBTW"] = $totaalBedragAanKlant;
+            $order["Bezorgen"] = $afhaalBezorgen;
+            $order["Besteldepizzas"] = $pizzaOrderLijst;
+
+            
+          
+            
+            
+            
+
+          
+
+        }
+        
+    ?>
 <style>
     body{
         background-image: url("pizza-2068272.jpg");
@@ -19,156 +146,57 @@
     text-align: center;
     max-width: 1000px;
     
-}
-</style>
+    }
+    label{
+        min-width: 600px;
+        max-width: 1000px;
+        padding: 5px;
+        
 
+    }
+    button{
+        max-width: 350px;
+        border: 2px solid saddlebrown;
+        box-shadow: 1px black;
+        text-align: center;
+        margin: 10px auto;
+        padding: 5px;
+
+    }  
+    button:hover {
+        background-color: violet;
+        font-size: 20px;
+    } 
+</style>
 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pizza di Silicone</title>
-</head>
-<body>
-    <div class="flex-container"> 
-    <h1 class="main_header">Pizza di Silicone</h1>
-    <p class="subtext">de lekkerste pizza's </p>
-    <form class="flex-container" action="" method="post">
-        <p class="naam">Naam:</p>
-        <input type="text" name="naam" placeholder="Uw naam" required>
-        <p class="adres">Adres:</p>
-        <input type="text" name="adres" placeholder="Uw adres" required>
-        <p class="postcode">Postcode:</p>
-        <input type="text" name="postcode" placeholder="Uw postcode" required>
-        <p class="woonplaats">Woonplaats:</p>
-        <input type="text" name="plaats" placeholder="Uw woonplaats" required>
-        <p class="datum">Besteldatum:</p>
-        <input type="date" name="besteldatum" placeholder="Besteldatum" required>
-        
-        <p class="bez-afh">Bezorgen of afhalen?</p>
-        <select name="afhaalBezorgen" value="afhaalBezorgen" required >
-            <option value="">[kies optie.....]</option>
-            <option value="bezorgen">Bezorgen</option>
-            <option value="afhalen">Afhalen</option>
-        </select>
-        <p class="welke-pizza">Welke pizza wil je?</p>
-        <select name="pizza" value="pizza" required>
-            <option value="">Kies een Pizza</option>
-            <option value="margherita">Pizza Margherita 12.50</option>
-            <option value="funghi">Pizza Funghi 12.50</option>
-            <option value="marina">Pizza Marina 13.95</option>
-            <option value="hawai">Pizza Hawai 11.50</option>
-            <option value="quattro">Pizza Quattro Formaggi 14.50</option>
-        </select>
-        <p> </p>
-        <button type="submit" name="submit">Bestellen</button>
-    </form>
-    </div>
-    <?php
-        if(isset($_POST["submit"])){
-            $datum= $order["Datum"];
-            #kosten en kortingen
-            $bezorgkosten = "5.00";
-            $maandagPizzaDag = "7.50";
-            $pizzaWeekend = "20.00";
-            $pizzaWeekendKorting = ".15";
-            $bezorgFlag = $order["Bezorgen"];
-            #My little helpers
-            #functie die berekend of er een korting van toepassing is.
-           function discount($datum, $maandagPizzaDag, $pizzaWeekend, $pizzaWeekendKorting, $totalPrice){
-            $day = date('D', $datum);
-            if ($day === 'Mon'){
-                $price = $maandagPizzaDag;
-                return $price;
-            }
-            elseif ($day === 'Sat' or $day === 'Sun'){
-                if ($totalPrice > $pizzaWeekend){
-                    $discount = $totalPrice / $pizzaWeekendKorting;
-                    $price = $totalPrice - $discount;
-                    return $price;
-                }
-                else{
-                    return $totalPrice;
-                }
-            }
-           }
-           #functie die bekijkt of bezorgkosten van toepassing zijn
-           function deliveryCost($totalPrice, $bezorgkosten, $bezorgFlag ){
-            if ($bezorgFlag === 'bezorgen'){
-                $price = $totalPrice + $bezorgkosten;
-                return $price; 
-            }
-           }
-           #functie die de totaal prijs berekend
-           function totalCalculator($pizzaOrderLijst, $pizzaPrijsLijst){
-            for each pizza in $pizzaOrderLijst;
-           }
-            
-
-
-            #prijzenlijst voor de pizza's
-            $pizzaPrijsLijst = array(
-                "soort" => "prijs",
-                "Margarita" => "12.50",
-                "Funghi" => "12.50",
-                "Marina" => "13.95",
-                "Hawai" => "11.50",
-                "Quattro Formaggi" => "14.50"
-            );
-            #bestelling van klant
-            $pizzaOrderLijst = array(
-                "soort" => "prijs",
-                "Margarita" => "0",
-                "Funghi" => "0",
-                "Marina" => "0",
-                "Hawai" => "0",
-                "Quattro Formaggi" => "0"
-            );
-
-            # maakt assosiated array voor de order aan
-            $order = array(
-                "Name" => $_POST["naam"],
-                "Address" => $_POST["adres"],
-                "Postcode" => $_POST["postcode"],
-                "City" => $_POST["plaats"],
-                "Bezorgen" => "",
-                "Datum" => date_create(),
-                "Tijd" => $_POST["tijd"],
-                "Option" => "",
-                "Prijs" => ""
-            );
-
-            $afhaalBezorgen = $_POST["afhaalBezorgen"];
-            $pizza = $_POST["pizza"];        
-            $order["Bezorgen"] = $afhaalBezorgen;
-            $order["Option"] = $pizza;
-
-            #zoek op de prijs in $pizzaPrijs en voeg deze to aan $order
-            if($pizza == "margarita" ){
-                $order["Prijs"] = $pizzaPrijsLijst["Margarita"];
-            }
-            elseif($pizza == "funghi"){
-                $order["Prijs"] = $pizzaPrijsLijst["Funghi"];
-            }
-            elseif($pizza == "marina"){
-                $order["Prijs"] = $pizzaPrijsLijst["Marina"];
-            }
-            elseif($pizza == "hawai"){
-                $order["Prijs"] = $pizzaPrijsLijst["Hawai"];
-            }
-            elseif($pizza == "quattro"){
-                $order["Prijs"] = $pizzaPrijsLijst["Quattro Formaggi"];
-            }
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pizza di Silicone</title>
+    </head>
+    <body>
+        <div class="flex-container"> 
+        <h1 class="main_header">Pizza di Silicone</h1>
+        <p class="subtext">de lekkerste pizza's </p>
+        <form class="flex-container" action="" method="post">
+            <label class="naam">Naam:</label>
+            <input type="text" name="naam" placeholder="Uw naam" required>
+            <label class="adres">Adres:</label>
+            <input type="text" name="adres" placeholder="Uw adres" required>
+            <label class="postcode">Postcode:</label>
+            <input type="text" name="postcode" placeholder="Uw postcode" required>
+            <label class="woonplaats">Woonplaats:</label>
+            <input type="text" name="plaats" placeholder="Uw woonplaats" required>
+       
             
             
-
-          
-
-        }
-        print_r($order)
-    ?>
-</body>
+            
+            <button type="submit" name="submit">Bestellen</button>
+        </form>
+        </div>
+    </body>
 </html>
--- php/pizza/sketchpad.php
+++ php/pizza/sketchpad.php
@@ -1,5 +1,104 @@
 <?php
 
-echo "10" * 8;
+$pizzaPrijsLijst = array(
+    #"soort" => "prijs",
+    "Margarita" => "12.50",
+    "Funghi" => "12.50",
+    "Marina" => "13.95",
+    "Hawai" => "11.50",
+    "Quattro Formaggi" => "14.50"
+);
 
+$pizzaOrderLijst = array(
+    #"soort" => "aantal",
+    "Margarita" => "1",
+    "Funghi" => "2",
+    "Marina" => "3",
+    "Hawai" => "4",
+    "Quattro Formaggi" => "5"
+);
+
+
+$prijzen = array_values($pizzaPrijsLijst);
+$aantal = array_values($pizzaOrderLijst);
+$totaalAantalPizzas = array_sum($aantal);
+
+function multi_arrays($array1, $array2) {
+    $array = array();
+    foreach($array1 as $index => $value) {
+        $array[$index] = isset($array2[$index]) ? $array2[$index] * $value : $value;
+    }
+    return $array;
+}
+
+$totaalBedragPerPizza = multi_arrays($prijzen, $aantal);
+print_r($totaalBedragPerPizza);
+echo ($totaalAantalPizzas);
+$datenow= date('d-m-y');
+
+echo   $datenow;
+
+$bestellijst = [
+    [
+        'Soort pizza' => 'Margarita', 'Prijs per stuk' => '12.50', 'Aantal' => '0', 'totaal' => "0"
+    ],
+    [
+        'Soort pizza' => 'Fungi', 'Prijs per stuk' => '12.50', 'Aantal' => '0', 'totaal' => "0"
+    ],
+
+    [
+        'Soort pizza' => 'Marina', 'Prijs per stuk' => '13.95', 'Aantal' => '0', 'totaal' => "0"
+    ],
+    [
+        'Soort pizza' => 'Hawai', 'Prijs per stuk' => '11.50', 'Aantal' => '0', 'totaal' => "0"
+    ],
+    [
+        'Soort pizza' => 'Quattro Formaggi', 'Prijs per stuk' => '14.50', 'Aantal' => '0', 'totaal' => "0"
+    ]
+    ];
+
+$klantBestand=[
+    
+    [
+        "ordernummer" =>  $_SERVER['REQUEST_TIME'],  'Naam'=> '', 'Adres' => '', 'Postcode' => '', 'Woonplaats' => '', 'Besteldatum' => date('d-m-y'), 'Bezorgen' => ''
+    ]
+];
+
+$test = array_values($bestellijst[0]);
+print_r($test);
 ?>
+
+<?php if (count($bestellijst) > 0): ?>
+<table>
+  <thead>
+    <tr>
+      <th><?php echo implode('</th><th>', array_keys(current($bestellijst))); ?></th>
+    </tr>
+  </thead>
+  <tbody>
+<?php foreach ($bestellijst as $row): array_map('htmlentities', $row); ?>
+    <tr>
+      <td><?php echo implode('</td><td>', $row); ?></td>
+    </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
+<?php endif; ?>
+
+
+<?php if (count($klantBestand) > 0): ?>
+<table>
+  <thead>
+    <tr>
+      <th><?php echo implode('</th><th>', array_keys(current($klantBestand))); ?></th>
+    </tr>
+  </thead>
+  <tbody>
+<?php foreach ($klantBestand as $row): array_map('htmlentities', $row); ?>
+    <tr>
+      <td><?php echo implode('</td><td>', $row); ?></td>
+    </tr>
+<?php endforeach; ?>
+  </tbody>
+</table>
+<?php endif; ?>
