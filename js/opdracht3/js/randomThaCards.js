a9412da2172c54c6fd2b9bf913e99e30de4dc077
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2022-12-08 14:45:59 +0100

    memory

--- js/opdracht3/js/randomThaCards.js
+++ js/opdracht3/js/randomThaCards.js
@@ -1,31 +1,27 @@
 
 function randomThaCards(){
     //get the length of the cardArray
-    let numberOfDifCards = cardArray.length;
+    let numberOfDifCards = cardArray.length - 1;
     let sortArray = [];
     let playArray = [];
     while (numberOfDifCards >= 0){
         //add cards to sort array * 2
         sortArray.push(numberOfDifCards);
         sortArray.push(numberOfDifCards);
-        alert(numberOfDifCards);
-        alert(sortArray);
+        
         numberOfDifCards--;
     }
+    
     //init let sortArrayLength
     let cardCounter = sortArray.length;
-    
+    console.log(sortArray);
     //while loop that adds cards to the play array and pops them from sortarray
-    while (cardCounter > 0){
-        
-        //get random number > its -1 because the length is not count 0
-        var randomNumber =getRandomInt(cardCounter -1);
-        //add card to playArray
-        playArray.push(cardArray[randomNumber]);
-        //pop randomNumber from sortArray
-        sortArray.pop(randomNumber);
-        cardCounter--;
-    }
+    while (sortArray.length >= 0){
+        var item = sortArray[Math.floor(Math.random()*sortArray.length)];
+        alert(item);
+        playArray.push(item);
+        sortArray.splice(item);}
+    
     
     return playArray;
 
