42810f2a2872d89e739027e55ca74bbf64d51bd0
Arnold Hilberink <104128195+NovioArnold@users.noreply.github.com>
2023-01-13 11:33:52 +0100

    opdracht 3 klaar

--- js/opdracht3/js/checkClickedCard.js
+++ js/opdracht3/js/checkClickedCard.js
@@ -1,10 +1,8 @@
-let sound =  [
-    './assets/audio/1fail.wav',
-    './assets/audio/2success.wav',
-    './assets/audio/3background.wav',
-]
-let rounds = count
 
+let rounds = count
+let backgroundMusic = './assets/audio/3background.wav'
+let soundSuccess = new Audio('./assets/audio/2success.wav')
+let soundFail = new Audio ('./assets/audio/1fail.wav')
 
 function checkClickedCard(cardNumber, cardId){
     //var backgroundMusic = document.createElement('audio');
@@ -31,14 +29,13 @@ function checkClickedCard(cardNumber, cardId){
     //reset routine
     
     if(userClick2!=999){
-        //var soundSuccess = document.createElement(sound[1]);
-        //var soundFail = document.createElement(sound[0]);
+        
         rounds = countClicks(rounds);
-        console.log(rounds);
         document.getElementById('rounds-played-data').innerHTML = rounds;
         if(userClick1==userClick2){
             document.getElementById("msg").innerHTML = "goed gedaan!";
-            //document.getElementById("soundSuccess").play();
+            
+            soundSuccess.play();
         }else{
             setTimeout(function(){
                 document.getElementById("msg").innerHTML = "Fout";
@@ -48,7 +45,8 @@ function checkClickedCard(cardNumber, cardId){
                 document.getElementById(userSelectedCard2).value=  "";
                 document.getElementById(userSelectedCard1).style = "background: url('./assets/back_of_card.jpg')";
                 document.getElementById(userSelectedCard2).style = "background: url('./assets/back_of_card.jpg')";
-                
+                soundFail.play();
+
             }, 1000);
             console.log("setTimeout() example...");
             
